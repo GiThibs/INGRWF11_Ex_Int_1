cb5bb316ec949e2188698d0f728b8348168a55e1
GiThibs <adelethibaut@gmail.com>
2023-12-19 15:25:26 +0100

    Edit Struct js + Add Radar Etud + Add Accueil Form

---
+++ js/formateur.js
@@ -0,0 +1,35 @@
+let etuds = document.querySelector('.listeetudiants')
+let addetud = document.querySelector('.addetudiant')
+let etudtoadd = document.querySelector('.etudtoadd')
+let etudiants = []
+let templateetud = ``
+
+let btncreatesession = document.querySelector('.btncreatesession')
+let formsession = document.querySelector('.formtoaddsession')
+
+
+addetud.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(etudtoadd.value == ""){
+        alert('Veuillez rentrez une adresse email valide :)')
+    } else {
+        console.log(etudtoadd.value)
+        etudiants.push(etudtoadd.value)
+        etuds.innerHTML = templateetud
+        etudiants.forEach(etud => {
+            templateetud +=`
+            <li class="listeetudiants__el">${etud}</li>
+            `
+        })
+        etuds.innerHTML = templateetud
+        templateetud = ``
+    }
+    etudtoadd.value = ""
+})
+
+btncreatesession.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e.target)
+
+    // create le formdata()
+})
--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-// import {$btnAfter, $btnBefore, chart} from "./js/evalEtudiant.js";
 
-// chart
-
-// $btnAfter();
-
-// $btnBefore();
